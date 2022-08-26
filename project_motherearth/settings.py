ecbdad1e70457f1c8fadd1617fead6bfc1a2d3c3
Fimber Uchenna Eze <uchenna631@gmail.com>
2022-08-26 22:58:58 +0000

    Troubleshooting

--- project_motherearth/settings.py
+++ project_motherearth/settings.py
@@ -83,26 +83,15 @@ WSGI_APPLICATION = 'project_motherearth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'OPTIONS': {
-#             'service': 'my_service',
-#             'passfile': '.my_pgpass',
-#         },
-#     }
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
 # Password validation
@@ -142,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CLUODINARY_STORAGE = {
+    "CLOUD_NAME": "uc-cloudinary",
+    "API_KEY" : "544474865635368",
+    "API_SECRET" : "pYToKOYJaUdujUNFD5oFIw6Sxbo"
+}
 
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
